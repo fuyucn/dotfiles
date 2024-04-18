e0477cbdc86ab2c06829215bb2b810df36387324
Ryan <6853947+fuyucn@users.noreply.github.com>
2024-04-18 00:13:46 -0700

    Update font-setup.sh

--- .config/font-setup.sh
+++ .config/font-setup.sh
@@ -4,9 +4,13 @@
 # sudo apt install fontconfig
 #
 # install JetBrains Mono Nerd Font
+# 
 echo "[-] Download fonts [-]"
 echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache -fv
 echo "done!"
+
+# BREW
+# brew tap homebrew/cask-fonts && brew install --cask font-jetbrains-mono-nerd-font
